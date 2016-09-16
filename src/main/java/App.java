f1fbe4278721d4d2ce7c2d441c6b3623f627c541
Nhat Hoang <hoangnh092185@gmail.com>
2016-09-16 14:14:12 -0700

    create a front end for word definition.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -20,11 +20,31 @@ public class App {
       }, new VelocityTemplateEngine());
 
       post("/", (request, response)->{
-      Map<String, Object> model = new HashMap<String, Object>();
-      Word aWord = new Word(request.queryParams("word"));
-      model.put("words", Word.all());
-      model.put("template", "templates/index.vtl");
-      return new ModelAndView(model, layout);
+        Map<String, Object> model = new HashMap<String, Object>();
+        Word aWord = new Word(request.queryParams("word"));
+        model.put("words", Word.all());
+        model.put("template", "templates/index.vtl");
+        return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
+
+      get("/word/:id", (request, response)->{
+        Map<String, Object> model = new HashMap<String, Object>();
+        Word currentWord = Word.find(Integer.parseInt(request.params(":id")));
+        model.put("word",currentWord);
+        model.put("definitions",currentWord.getDefinitions());
+        model.put("template", "templates/word.vtl");
+        return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
+
+      post("/word/:id", (request, response)->{
+        Map<String, Object> model = new HashMap<String, Object>();
+        Word currentWord = Word.find(Integer.parseInt(request.queryParams("definitionId")));
+        Definition newDefinition = new Definition(request.queryParams("definition"));
+        currentWord.addDefinition(newDefinition);
+        model.put("word",currentWord);
+        model.put("definitions",currentWord.getDefinitions());
+        model.put("template", "templates/word.vtl");
+        return new ModelAndView(model, layout);
       }, new VelocityTemplateEngine());
   }
 }
